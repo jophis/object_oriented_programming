a2dced624e209002926b5dc44fefab57b558a76a
Joseph Longmoore <josephlongmoore@Josephs-MacBook-Air.local>
2014-03-27 13:13:46 -0400

    step 6 yet again

--- people.rb
+++ people.rb
@@ -1,7 +1,31 @@
-class Student
+class Person
+	def initialize(name)
+		@name = name
+	end
 
+	def greeting
+		puts "Hi, my names is #{name}"
+	end
 end
+
+class Student
+	include Person
+	
+	def learn
+		puts "I get it!"
+	end
+end
+
 
 class Instructor
+	include Person
+
+	def teach
+		puts "Everything in Ruby is an Object"
+	end 
 
 end
+
+p1 = Instructor.new("Chris")
+puts p1.greeting 
+
